8773cb7191793034d8884fb5e5a75ddaf4ef8f74
Azamat1855 <azamatshomaksudov7@gmail.com>
2024-08-06 11:21:20 +0500

    ASD

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { IoCameraOutline } from "react-icons/io5";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { MdReportGmailerrorred } from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
 
 const Footer = () => {
   return (
     <div>
-      
+      <div className="btm-nav">
+  <button>
+  <IoCameraOutline />
+  </button>
+  <button className="active">
+  <IoMdAddCircleOutline />
+  </button>
+  <button>
+  <MdReportGmailerrorred />
+  </button>
+  <button>
+  <CgProfile />
+  </button>
+</div>
     </div>
   )
 }
